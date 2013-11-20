cf1c106c28b7937a56026f389f75f17f476b5bb1
James Maniscalco <maniscalco.jt@gmail.com>
2013-11-20 19:25:05 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Divvyup::Application.config.secret_key_base = 'ab38bec24ef07c14897b6e097c257b5388c6705d39e4104ca0d5c92be4d28375c1946d4ae4d80a4e7555e9559794443d31d455d7bc3e32666ff21c11d9de2864'
