c852cb7c1675567bc5beac8d7d656502885f44d7
James Maniscalco <maniscalco.jt@gmail.com>
2013-11-21 16:27:23 +0100

    added user creation form

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,6 +2,27 @@ class PeopleController < ApplicationController
 
 
   def index
-    @people = Person.all()
+    @people = Person.all
+  end
+
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.create(person_params)
+
+    if @person.save
+      redirect_to @index
+    else
+      render 'new'
+    end
+    
+  end
+
+
+
+  def person_params
+    params.require(:person).permit(:name)
   end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,7 @@
 class Person < ActiveRecord::Base
+  # validations
+  validates :name, length: { minimum: 1 }
+
   # relations
   has_many :uses
   has_many :items
