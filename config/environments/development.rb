13d7b7b6dc5d227dfaa4e4f11188f08fbee08245
James Maniscalco <maniscalco.jt@gmail.com>
2013-12-17 15:41:04 +0100

    Added devise (gem only), moved page title to separate yield in template, added flash notice and alert

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Divvyup::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  # devise stuff
+  config.action_mailer.default_url_options = { :host => 'localhost:8080' }
 end
