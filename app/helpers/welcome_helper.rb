5c337806f8973dcfa5d7e70318017cd5b2446084
James Maniscalco <maniscalco.jt@gmail.com>
2013-12-19 17:29:39 +0100

    moved sign-in form to partial, included it in welcome page

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,13 @@
 module WelcomeHelper
+  def resource_name
+    :group
+  end
+
+  def resource
+    @resource ||= Group.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:group]
+  end
 end
